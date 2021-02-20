b5e707e3e1009011d19ccef85ef86f07ca3db7ec
315714 <3157143125@qq.com>
2021-02-20 16:25:08 +0800

    试验

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "widget.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Widget w;
+    w.show();
+    cout << "你好" << endl;
+
+    return a.exec();
+}
